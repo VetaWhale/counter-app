99f2c1ef2636b78bbfa8847297d98d2d7cd25afa
Elizaveta Shkimbova <veta-whale@yandex.ru>
2023-05-02 13:28:20 +0300

    added styles and functional

---
+++ index.js
@@ -0,0 +1,24 @@
+let counter = 0;
+let counterContent = document.querySelector(".counter-content");
+let counterButton = document.querySelector(".counter-button");
+let counterReset = document.querySelector(".counter-button-reset");
+let counterText = document.querySelector(".counter-text");
+
+counterButton.addEventListener("click", function () {
+  counter = counter + 1;
+  counterContent.innerText = counter;
+  if (counter >= 5 & counter < 8) {
+    counterText.innerText = "Молодец! Еще немного, еще чуть-чуть!";
+  } else if (counter >= 8 & counter < 11) {
+    console.log("Больше 8ми");
+    counterText.innerText = "Ты достиг цели! Поздравляю!";
+  } else if (counter > 11) {
+    counterText.innerText = "Кажется, это уже перебор! Остановись!";
+  }
+});
+
+counterReset.addEventListener("click", function () {
+  counter = 0;
+  counterContent.innerText = counter;
+  counterText.innerText = 'Норма воды в день: 8 стаканов';
+});
